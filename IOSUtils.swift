9d9d3306bf8dca3cc664a67a024989cceaa385b8
Diogo Henrique <diogjp10@gmail.com>
2016-05-04 10:20:43 +0100

    Added files via upload

---
+++ IOSUtils.swift
@@ -0,0 +1,15 @@
+//
+//  IOSUtils.swift
+//
+//
+//  Created by Diogo on 04/05/2016.
+//  Copyright © 2016 Merino. All rights reserved.
+//
+
+import Foundation
+
+class IOSUtils {
+    
+   
+    
+}
