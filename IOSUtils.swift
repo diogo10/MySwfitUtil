6f14843d5a9f03e57316546a476525f05b54be17
Diogo Henrique <diogjp10@gmail.com>
2016-05-05 10:32:16 +0100

    add a new getIp method

--- IOSUtils.swift
+++ IOSUtils.swift
@@ -10,6 +10,24 @@ import Foundation
 
 class IOSUtils {
     
-   
+   func getIp() -> String? {
+        let host = CFHostCreateWithName(nil,"www.google.com").takeRetainedValue()
+        CFHostStartInfoResolution(host, .Addresses, nil)
+        var success: DarwinBoolean = false
+        if let addresses = CFHostGetAddressing(host, &success)?.takeUnretainedValue() as NSArray?,
+            let theAddress = addresses.firstObject as? NSData {
+            var hostname = [CChar](count: Int(NI_MAXHOST), repeatedValue: 0)
+            if getnameinfo(UnsafePointer(theAddress.bytes), socklen_t(theAddress.length),
+                           &hostname, socklen_t(hostname.count), nil, 0, NI_NUMERICHOST) == 0 {
+                if let numAddress = String.fromCString(hostname) {
+                    return numAddress
+                }
+            }
+            return ""
+        }else{
+            return ""
+        }
+        
+    }
     
 }
