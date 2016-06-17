482c5e8d634121da155947ce83c5ee09034e8b5e
Diogo Henrique <diogjp10@gmail.com>
2016-06-17 10:30:28 +0100

    extension

---
+++ AppDelegate.swift
@@ -0,0 +1,141 @@
+//
+//  AppDelegate.swift
+//  JampaBusIOS
+//
+//  Created by Diogo Henrique on 30/08/2015.
+//  Copyright (c) 2015 Diogo Henrique. All rights reserved.
+//
+
+import UIKit
+
+//GLOBAL
+var storyBoard:UIStoryboard!
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        storyBoard = UIStoryboard(name: "Main", bundle:nil)
+        self.window!.tintColor = UIColor(hexString: "#F4AC0D") //UIColor.init(red: 244, green: 172, blue: 12, alpha: 1)
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
+extension UIColor {
+    convenience init(hexString: String) {
+        let hex = hexString.stringByTrimmingCharactersInSet(NSCharacterSet.alphanumericCharacterSet().invertedSet)
+        var int = UInt32()
+        NSScanner(string: hex).scanHexInt(&int)
+        let a, r, g, b: UInt32
+        switch hex.characters.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+}
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+extension String
+{
+    func trim() -> String
+    {
+        return self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+    }
+    
+    var localized: String {
+        return NSLocalizedString(self, tableName: nil, bundle: NSBundle.mainBundle(), value: "", comment: "")
+    }
+}
+
+extension NSDate
+{
+    func hour() -> Int
+    {
+        //Get Hour
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.Hour, fromDate: self)
+        let hour = components.hour
+        
+        //Return Hour
+        return hour
+    }
+    
+    
+    func minute() -> Int
+    {
+        //Get Minute
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.Minute, fromDate: self)
+        let minute = components.minute
+        
+        //Return Minute
+        return minute
+    }
+    
+    func toShortTimeString() -> String
+    {
+        //Get Short Time String
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "dd/MM/yyy"
+        let timeString = formatter.stringFromDate(self)
+        
+        //Return Short Time String
+        return timeString
+    }
+    
+    func toMyLongTimeString() -> String
+    {
+        //Get Short Time String
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "dd/MM/yyy HH:mm"
+        let timeString = formatter.stringFromDate(self)
+        
+        //Return Short Time String
+        return timeString
+    }
+}
